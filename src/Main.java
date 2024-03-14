0020d11b19d6af888179e0b8bbc45d3f0ac99050
Vanbq1204 <157662235+Vanbq1204@users.noreply.github.com>
2024-03-14 07:28:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Song", "this is a song", "1234",123,290);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Book", "This is a book", "5678", 20.99f, "John Doe", 300, "Fiction");
         book.showInfo();
-
     }
 }
