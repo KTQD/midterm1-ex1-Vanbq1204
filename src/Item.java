43b0de01289fda3518f4c54708b48e2b878fdc31
Vanbq1204 <157662235+Vanbq1204@users.noreply.github.com>
2024-03-14 07:37:37 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,20 +1,23 @@
-abstract class Item {
+public abstract class Item {
     public String name;
-    private String description ;
+    private String description;
     protected String ID;
     float price;
-    public String getDescription(){
-        return description;
-    }
-    public String setDescription(){
-        return this.description= description;
-    }
-    public abstract void showInfo();
 
-    public Item (String name, String description, String ID,float price){
+    public Item(String name, String description, String ID, float price) {
         this.name = name;
         this.description = description;
         this.ID = ID;
         this.price = price;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void showInfo();
 }
