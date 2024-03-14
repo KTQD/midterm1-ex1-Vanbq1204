d911e2fa4d5df4d5e7e4a6f2c8caad06e0406ab8
Vanbq1204 <157662235+Vanbq1204@users.noreply.github.com>
2024-03-14 07:38:16 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,24 +1,24 @@
-class Book extends Item {
+public class Book extends Item {
     private String author;
-    private int numberOfpages;
+    private int numberOfPages;
     private String genre;
 
-    public Book(String name, String description, String ID, float price,
-                String author, int numberOfpages, String genre) {
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
         super(name, description, ID, price);
         this.author = author;
-        this.numberOfpages = numberOfpages;
+        this.numberOfPages = numberOfPages;
         this.genre = genre;
     }
+
     @Override
-    public void showInfo(){
-            System.out.println("Book Information:");
-            System.out.println("Name: " + name);
-            System.out.println("Description: " + getDescription());
-            System.out.println("ID: " + ID);
-            System.out.println("Price: " + price);
-            System.out.println("Author: " + author);
-            System.out.println("Number of Pages: " + numberOfpages);
-            System.out.println("Genre: " + genre);
-        }
+    public void showInfo() {
+        System.out.println("Book Info:");
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Author: " + author);
+        System.out.println("Number of Pages: " + numberOfPages);
+        System.out.println("Genre: " + genre);
+    }
 }
